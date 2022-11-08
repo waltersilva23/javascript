4863d911c8a0cf8b44ea1b1c23365c67e3b79c63
Walter Silva <108630688+waltersilva23@users.noreply.github.com>
2022-11-07 21:22:04 -0300

    delete

--- projetos/index.js
+++
@@ -1,7 +0,0 @@
-var readlineSync = require('readline-sync');
-var nota1 = readlineSync.question('\nDigite a primeira nota: ');
-var nota2 = readlineSync.question('\nDigite a segunda nota: ');
-var soma = parseFloat(nota1) + parseFloat(nota2);
-var falta = 21 - soma;
-
-console.log("\nA nota mínima que precisa tirar para ficar com a média 7 é: "  +  falta.toFixed(2));
